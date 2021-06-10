2b94f806096e6f9a34706ac663fb4b31a260ae75
arundhaas1 <arunram.9215@gmail.com>
2021-06-09 22:38:01 -0700

    add login page

---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            <img alt="" src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-symbol-500x152.jpg"></img>
+            <a>
+              <button className="grow">LOGIN WITH SPOTIFY</button>
+            </a>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/Components/Spotify.js
@@ -0,0 +1,5 @@
+const authEndPoint ="https://accounts.spotify.com/authorize"
+
+const redirectUrl = "http://localhost:3000"
+
+const clientId ="27d50fa0c6bd49489d24d8c27b2457c8"
